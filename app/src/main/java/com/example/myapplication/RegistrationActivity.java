e572cbc6d4fc8a675af86f7ee570deba4e5acda6
arrfhkl <94587405+arrfhkl@users.noreply.github.com>
2023-10-19 17:14:03 +0800

    rgdg

--- app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -1,37 +1,25 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import com.google.firebase.firestore.SetOptions;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private RadioGroup radioGroup;
-    private RadioButton radioCompany;
-    private RadioButton radioIndividual;
-    private EditText companyNameEditText;
-    private EditText fullNameEditText;
-    private EditText emailEditText;
-    private EditText phoneEditText;
-    private EditText passwordEditText;
-    private EditText confirmPasswordEditText;
-    private Button registerButton;
+    private EditText riderNameEditText;
+    private EditText riderEmailEditText;
+    private EditText riderPhoneEditText;
+    private EditText riderPasswordEditText;
+    private EditText riderConfirmPasswordEditText;
+    private Button riderRegisterButton;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
@@ -40,61 +28,46 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        // Initialize Firebase Authentication
-        FirebaseApp.initializeApp(this);
+        // Initialize Firebase Authentication and Firestore
         mAuth = FirebaseAuth.getInstance();
-
-        // Initialize Firebase Firestore
         db = FirebaseFirestore.getInstance();
 
-        radioGroup = findViewById(R.id.radioGroup);
-        radioCompany = findViewById(R.id.radioCompany);
-        radioIndividual = findViewById(R.id.radioIndividual);
-        companyNameEditText = findViewById(R.id.companyNameEditText);
-        fullNameEditText = findViewById(R.id.fullNameEditText);
-        emailEditText = findViewById(R.id.emailEditText);
-        phoneEditText = findViewById(R.id.phoneEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
-        registerButton = findViewById(R.id.registerButton);
+        riderNameEditText = findViewById(R.id.riderNameEditText);
+        riderEmailEditText = findViewById(R.id.riderEmailEditText);
+        riderPhoneEditText = findViewById(R.id.riderPhoneEditText);
+        riderPasswordEditText = findViewById(R.id.riderPasswordEditText);
+        riderConfirmPasswordEditText = findViewById(R.id.riderConfirmPasswordEditText);
+        riderRegisterButton = findViewById(R.id.riderRegisterButton);
 
-        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            if (checkedId == R.id.radioCompany) {
-                companyNameEditText.setVisibility(View.VISIBLE);
-            } else if (checkedId == R.id.radioIndividual) {
-                companyNameEditText.setVisibility(View.GONE);
-            }
-        });
+        riderRegisterButton.setOnClickListener(v -> {
+            String riderName = riderNameEditText.getText().toString();
+            String riderEmail = riderEmailEditText.getText().toString();
+            String riderPhone = riderPhoneEditText.getText().toString();
+            String riderPassword = riderPasswordEditText.getText().toString();
+            String riderConfirmPassword = riderConfirmPasswordEditText.getText().toString();
 
-        registerButton.setOnClickListener(v -> {
-            String selectedType = radioCompany.isChecked() ? "Company" : "Individual";
-            String companyName = companyNameEditText.getText().toString();
-            String fullName = fullNameEditText.getText().toString();
-            String email = emailEditText.getText().toString();
-            String phone = phoneEditText.getText().toString();
-            String password = passwordEditText.getText().toString();
-            String confirmPassword = confirmPasswordEditText.getText().toString();
-
-            if (validateInput(fullName, email, phone, password, confirmPassword)) {
-                mAuth.createUserWithEmailAndPassword(email, password)
+            if (validateInput(riderName, riderEmail, riderPhone, riderPassword, riderConfirmPassword)) {
+                mAuth.createUserWithEmailAndPassword(riderEmail, riderPassword)
                         .addOnCompleteListener(this, task -> {
                             if (task.isSuccessful()) {
-                                // The user has been registered with email and password in Firebase Authentication.
                                 FirebaseUser currentUser = mAuth.getCurrentUser();
                                 if (currentUser != null) {
                                     String userId = currentUser.getUid();
-                                    User newUser = new User(selectedType, companyName, fullName, phone);
 
-                                    // Save additional details in Firestore
-                                    DocumentReference userRef = db.collection("users").document(userId);
-                                    userRef.set(newUser)
+                                    // Create a Rider object with the details to be saved in Firestore
+                                    Rider newRider = new Rider(riderName, riderPhone);
+
+                                    // Save additional rider details in Firestore
+                                    db.collection("riders")
+                                            .document(userId)
+                                            .set(newRider, SetOptions.merge())
                                             .addOnSuccessListener(aVoid -> {
                                                 mAuth.signOut();
                                                 Toast.makeText(RegistrationActivity.this, "Registration successful! You are now signed out.", Toast.LENGTH_SHORT).show();
                                                 finish();
                                             })
                                             .addOnFailureListener(e -> {
-                                                Toast.makeText(RegistrationActivity.this, "Error saving user data in Firestore. Please try again.", Toast.LENGTH_SHORT).show();
+                                                Toast.makeText(RegistrationActivity.this, "Error saving rider data in Firestore. Please try again.", Toast.LENGTH_SHORT).show();
                                             });
                                 }
                             } else {
@@ -105,28 +78,28 @@ public class RegistrationActivity extends AppCompatActivity {
         });
     }
 
-    private boolean validateInput(String fullName, String email, String phone, String password, String confirmPassword) {
-        if (fullName.isEmpty() || email.isEmpty() || phone.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+    private boolean validateInput(String riderName, String riderEmail, String riderPhone, String riderPassword, String riderConfirmPassword) {
+        if (riderName.isEmpty() || riderEmail.isEmpty() || riderPhone.isEmpty() || riderPassword.isEmpty() || riderConfirmPassword.isEmpty()) {
             Toast.makeText(RegistrationActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(riderEmail).matches()) {
             Toast.makeText(RegistrationActivity.this, "Invalid email format", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        if (phone.length() != 10) {
+        if (riderPhone.length() != 10) {
             Toast.makeText(RegistrationActivity.this, "Phone number should be 10 digits", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        if (password.length() < 6) {
+        if (riderPassword.length() < 6) {
             Toast.makeText(RegistrationActivity.this, "Password should be at least 6 characters", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        if (!password.equals(confirmPassword)) {
+        if (!riderPassword.equals(riderConfirmPassword)) {
             Toast.makeText(RegistrationActivity.this, "Password and confirm password do not match", Toast.LENGTH_SHORT).show();
             return false;
         }
@@ -134,68 +107,23 @@ public class RegistrationActivity extends AppCompatActivity {
         return true;
     }
 
-    public class User {
-        private String userType;
-        private String companyName;
-        private String fullName;
+    public class Rider {
+        private String name;
         private String phone;
 
-        public User() {
+        public Rider() {
             // Default constructor required for Firestore
         }
 
-        public User(String userType, String companyName, String fullName, String phone) {
-            this.userType = userType;
-            this.companyName = companyName;
-            this.fullName = fullName;
+        public Rider(String name, String phone) {
+            this.name = name;
             this.phone = phone;
-        }
-
-        // Getter and setter methods for your fields (optional)
-
-        public String getUserType() {
-            return userType;
-        }
-
-        public void setUserType(String userType) {
-            this.userType = userType;
-        }
-
-        public String getCompanyName() {
-            return companyName;
-        }
-
-        public void setCompanyName(String companyName) {
-            this.companyName = companyName;
-        }
-
-        public String getFullName() {
-            return fullName;
-        }
-
-        public void setFullName(String fullName) {
-            this.fullName = fullName;
-        }
-
-        public String getPhone() {
-            return phone;
-        }
-
-        public void setPhone(String phone) {
-            this.phone = phone;
-        }
-
-        // You can also create a method to convert the data class to a Map for Firestore
-        public Map<String, Object> toMap() {
-            Map<String, Object> map = new HashMap<>();
-            map.put("userType", userType);
-            map.put("companyName", companyName);
-            map.put("fullName", fullName);
-            map.put("phone", phone);
-            return map;
         }
     }
 }
+
+
+
 
 
 
