72151396afb9df4324e7fb651fc03c7823cedea5
Aimaniac <95029143+Aimaniac@users.noreply.github.com>
2023-10-27 11:26:52 +0800

    oi

--- app/src/main/java/com/example/myapplication/UsernameLoginFragment.java
+++ app/src/main/java/com/example/myapplication/UsernameLoginFragment.java
@@ -9,15 +9,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UsernameLoginFragment extends Fragment {
 
-    private EditText usernameEditText;
+    private EditText emailEditText;
     private EditText passwordEditText;
     private Button loginButton;
     private TextView forgotPasswordTextView;
@@ -30,7 +36,7 @@ public class UsernameLoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_username_login, container, false);
 
-        usernameEditText = view.findViewById(R.id.usernameEditText);
+        emailEditText = view.findViewById(R.id.emailEditText);
         passwordEditText = view.findViewById(R.id.passwordEditText);
         loginButton = view.findViewById(R.id.loginButton);
         forgotPasswordTextView = view.findViewById(R.id.forgotPasswordTextView);
@@ -39,17 +45,45 @@ public class UsernameLoginFragment extends Fragment {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Add code to handle login here
-                Intent intent = new Intent(getActivity(), UserHomePageActivity.class);
-                startActivity(intent);
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                // Check if email and password are not empty
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getActivity(), "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Login successful
+                                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                                    if (currentUser != null) {
+                                        String userEmail = currentUser.getEmail(); // Get user email
+                                        Toast.makeText(getActivity(), "Logged in as: " + userEmail, Toast.LENGTH_SHORT).show();
+
+                                        // Redirect to the home page or your desired activity
+                                        Intent intent = new Intent(getActivity(), HomePageActivity.class);
+                                        startActivity(intent);
+                                    }
+                                } else {
+                                    // Login failed
+                                    Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 
         forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Navigate to the forgot password page when clicked
-                // You can replace ForgotPasswordActivity.class with your actual activity
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
                 Intent intent = new Intent(getActivity(), UserForgotPasswordActivity.class);
                 startActivity(intent);
             }
